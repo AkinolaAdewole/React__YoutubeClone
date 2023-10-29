f9e45928b4f7c8c79a67f89827ec9c8636301af2
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-29 18:16:47 +0100

    Youtube clone

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,21 +1,18 @@
-const axios = require('axios');
+import axios from "axios";
 
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 const options = {
-  method: 'GET',
-  url: 'https://youtube-v31.p.rapidapi.com/captions',
   params: {
-    part: 'snippet',
-    videoId: 'M7FIvfx5J10'
+    maxResults:50,
   },
   headers: {
-    'X-RapidAPI-Key': '57b0c9d622msh5b18b9a05fdf052p1b63f7jsn1cfeb576c483',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  
+    return data;
 }
