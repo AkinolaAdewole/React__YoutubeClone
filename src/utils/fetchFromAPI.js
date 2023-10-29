c4a58b22a8a5e758dc926cc90a695734629849df
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-29 20:53:49 +0100

    Youtube clone

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -9,7 +9,7 @@ const options = {
     videoId: 'M7FIvfx5J10'
   },
   headers: {
-    'X-RapidAPI-Key': '57b0c9d622msh5b18b9a05fdf052p1b63f7jsn1cfeb576c483',
+    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
