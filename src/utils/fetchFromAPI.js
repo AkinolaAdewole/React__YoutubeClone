7edf53baae12f59d2d06af4620eb0f014a2c62cf
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-29 19:04:18 +0100

    Youtube clone

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,18 +1,23 @@
 import axios from "axios";
 
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+
 const options = {
+  method: 'GET',
   params: {
-    maxResults:50,
+    part: 'snippet',
+    videoId: 'M7FIvfx5J10'
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '57b0c9d622msh5b18b9a05fdf052p1b63f7jsn1cfeb576c483',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
+
 
 export const fetchFromAPI = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
   
     return data;
 }
+
